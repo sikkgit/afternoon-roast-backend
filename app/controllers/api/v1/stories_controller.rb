3ab1f2a249789b95da340019e1b0da1815e7a747
vuonga1103 <vuonga1103@yahoo.com>
2020-11-27 16:45:28 -0500

    edit story

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -17,13 +17,37 @@ module Api
         end
       end
 
+      def update
+        begin
+          @story = Story.find_by(id: params[:id])
+
+          if @story
+            @story.update(title: params[:title])
+            @story.sanitize_and_update_html(params[:html])
+
+            tag = Tag.find_or_create_by(name: params[:tag])
+            @story.update(tag: tag)
+            @story.save
+
+            @story.lyra_connection('patch', @story.uuid)
+
+            render json: @story
+          else
+            render json: {error: 'Error: Story update unsuccessful'}
+          end
+          
+        rescue => exception
+          render json: {error: 'Error: Story update unsuccessful'}
+        end
+      end
+
       def create
         begin
           tag = Tag.find_or_create_by(name: params[:tag])        
           @story = Story.create(title: params[:title], tag: tag)
 
           @story.add_sanitized_html(params[:html])
-          @story.post_to_lyra()
+          @story.lyra_connection('post')
 
           render json: @story
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -12,11 +12,29 @@ class Story < ApplicationRecord
     self.save
   end
 
-  def post_to_lyra
-    url = URI(@@base_url + "/stories")
+  def sanitize_and_update_html(html)
+    sanitized_html = ActionController::Base.helpers.sanitize(html)
+    self.update(html: sanitized_html)
+    self.save
+  end
+
+  def lyra_connection(http_method, uuid = nil)
+    uri_string = @@base_url + "/stories"
+    url = URI(uri_string)
+
+    if http_method == 'patch'
+      url = URI(uri_string + "/" + uuid)
+    end
+    
     https = Net::HTTP.new(url.host, url.port); 
     https.use_ssl = true
-    request = Net::HTTP::Post.new(url)
+
+    if http_method == 'post'
+      request = Net::HTTP::Post.new(url)
+    elsif http_method == 'patch'
+      request = Net::HTTP::Patch.new(url)
+    end
+
     request["Authorization"] = "Bearer "+ ENV['LYRA_API_KEY']
 
     form_data = [['html', self.html],['title', self.title]]
@@ -25,10 +43,10 @@ class Story < ApplicationRecord
 
     parsed_response = JSON.parse(response.body)
     uuid = parsed_response["data"]["id"]
-
     self.uuid = uuid
     self.save
   end
+
 end
 
 
